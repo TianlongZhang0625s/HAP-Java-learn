ce7f9609006251af76ddfbd9d93efb52156392fa
conterZhangtianlong <10108526579@qq.com>
2020-08-29 16:27:14 +0800

    first upload for HAP-Java

--- src/main/java/com/simpleImplment/hap/demonstration/Main.java
+++ src/main/java/com/simpleImplment/hap/demonstration/Main.java
@@ -1,4 +1,57 @@
 package com.simpleImplment.hap.demonstration;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import io.github.hapjava.server.impl.HomekitRoot;
+import io.github.hapjava.server.impl.HomekitServer;
 
 public class Main {
+
+    private static final int PORT = 9123;
+
+    public static void main(String[] args) {
+        try {
+            File authFile = new File("auth-state.bin");
+            MockAuthInfo mockAuth;
+            if (authFile.exists()) {
+                FileInputStream fileInputStream = new FileInputStream(authFile);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                try {
+                    System.out.println("Using persisted auth");
+                    AuthState authState = (AuthState) objectInputStream.readObject();
+                    mockAuth = new MockAuthInfo(authState);
+                } finally {
+                    objectInputStream.close();
+                }
+            } else {
+                mockAuth = new MockAuthInfo();
+            }
+
+            HomekitServer homekit = new HomekitServer(PORT);
+            HomekitRoot bridge = homekit.createBridge(mockAuth, "Test Bridge", "TestBridge, Inc.", "G6", "111abe234", "1.1", "1.2");
+
+
+            mockAuth.onChange(state -> {
+                try {
+                    System.out.println("State has changed! Writing");
+                    FileOutputStream fileOutputStream = new FileOutputStream(authFile);
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                    objectOutputStream.writeObject(state);
+                    objectOutputStream.flush();
+                    objectOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            bridge.addAccessory(new MockSwitch());
+            bridge.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/main/java/com/simpleImplment/hap/demonstration/MockSwitch.java
+++ src/main/java/com/simpleImplment/hap/demonstration/MockSwitch.java
@@ -1,4 +1,80 @@
 package com.simpleImplment.hap.demonstration;
 
-public class MockSwitch {
+import java.util.concurrent.CompletableFuture;
+
+import io.github.hapjava.accessories.LightbulbAccessory;
+import io.github.hapjava.accessories.optionalcharacteristic.AccessoryWithHardwareRevision;
+import io.github.hapjava.characteristics.HomekitCharacteristicChangeCallback;
+
+public class MockSwitch implements LightbulbAccessory, AccessoryWithHardwareRevision {
+
+    private boolean powerState = false;
+    private HomekitCharacteristicChangeCallback subscribeCallback = null;
+
+    @Override
+    public int getId() {
+        return 2;
+    }
+
+    @Override
+    public CompletableFuture<String> getName() {
+        return CompletableFuture.completedFuture("Test Switch Name");
+    }
+
+    @Override
+    public void identify() {
+        System.out.println("Identifying light");
+    }
+
+    @Override
+    public CompletableFuture<String> getSerialNumber() {
+        return CompletableFuture.completedFuture("Test SwitchSN");
+    }
+
+    @Override
+    public CompletableFuture<String> getModel() {
+        return CompletableFuture.completedFuture("TestSwitch Model");
+    }
+
+    @Override
+    public CompletableFuture<String> getManufacturer() {
+        return CompletableFuture.completedFuture("Test SwitchManufacturer");
+    }
+
+    @Override
+    public CompletableFuture<String> getFirmwareRevision() {
+        return CompletableFuture.completedFuture("Test Switch Firmware");
+    }
+
+    @Override
+    public CompletableFuture<Boolean> getLightbulbPowerState() {
+        return CompletableFuture.completedFuture(powerState);
+    }
+
+    @Override
+    public CompletableFuture<Void> setLightbulbPowerState(boolean powerState)
+            throws Exception {
+        this.powerState = powerState;
+        if (subscribeCallback != null) {
+            subscribeCallback.changed();
+        }
+        System.out.println("The lightbulb is now "+(powerState ? "on" : "off"));
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public void subscribeLightbulbPowerState(
+            HomekitCharacteristicChangeCallback callback) {
+        this.subscribeCallback = callback;
+    }
+
+    @Override
+    public void unsubscribeLightbulbPowerState() {
+        this.subscribeCallback = null;
+    }
+
+    @Override
+    public CompletableFuture<String> getHardwareRevision() {
+        return CompletableFuture.completedFuture("Test Switch Hardware");
+    }
 }
